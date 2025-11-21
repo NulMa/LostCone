1356ad1f99e7629f25583c3bece2074e6c2cb8bf
rlaals0203 <scientistmin@gmail.com>
2025-11-21 22:02:28 +0900

    feat : iris out prefab

--- Assets/01_Scripts/Achive/Achives/IrisoutAchive.cs
+++ Assets/01_Scripts/Achive/Achives/IrisoutAchive.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Code.Map;
 using DG.Tweening;
 using PaperFlower.Core;
@@ -12,7 +13,7 @@ namespace Code.Achive
         [SerializeField] private Image fadeImage;
         [SerializeField] private Image mask;
         [SerializeField] private TextMeshProUGUI irisOutText;
-        [SerializeField] private float textDuration = 1f;
+        [SerializeField] private float textDuration = 0.7f;
         [SerializeField] private Animator irisOutAnimator;
         
         private readonly int _circleSizeHash = Shader.PropertyToID("_CircleSize");
@@ -29,19 +30,36 @@ namespace Code.Achive
         {
             irisOutAnimator.enabled = true;
             await Awaitable.WaitForSecondsAsync(0.2f);
-            
+
             Sequence seq = DOTween.Sequence();
             mask.gameObject.SetActive(true);
             mask.transform.localScale = Vector3.zero;
 
             fadeImage.material.SetFloat(_circleSizeHash, 2f);
             seq.Append(fadeImage.material.DOFloat(0f, _circleSizeHash, 1f))
-                .AppendInterval(0.25f)
-                .Append(irisOutText.DOText("I  I  I  I", textDuration))
+                .AppendInterval(0.25f);
+
+            for (int i = 1; i <= 4; i++)
+            {
+                int idx = i;
+                seq.AppendCallback(() => {
+                    irisOutText.text = string.Join("  ",
+                        Enumerable.Repeat("I,", idx));
+                });
+                seq.AppendInterval(textDuration / 4f);
+            }
+
+            seq.AppendCallback(() =>
+                {
+                    irisOutText.transform.localScale = Vector3.one * 1.25f;
+                    irisOutText.text = "IRIS OUT";
+                })
                 .AppendInterval(1f)
                 .Append(mask.transform.DOScale(15f, 1f))
-                .AppendCallback(() => {
+                .AppendCallback(() =>
+                {
                     AchiveManager.instance.SetAchiveClear(message);
+                    irisOutText.transform.localScale = Vector3.one;
                     irisOutText.text = string.Empty;
                     mask.gameObject.SetActive(false);
                 })
