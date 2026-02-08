4e0690d539737f890d737350651a50bd2a9a6dd0
rlaals0203 <scientistmin@gmail.com>
2026-02-08 21:02:50 +0900

    feat : sound system

---
+++ Assets/01_Scripts/SoundSystem/SoundEvents.cs
@@ -0,0 +1,33 @@
+using Blade.SoundSystem;
+using PaperFlower.Core;
+
+public static class SoundEvents
+{
+    public static PlaySFXEvent PlaySFXEvent = new PlaySFXEvent();
+    public static StopSoundEvent StopSoundEvent = new StopSoundEvent();
+}
+
+public class PlaySFXEvent : GameEvent
+{
+    public SoundSO clip;
+    public int channel;
+
+    public PlaySFXEvent Initialize(SoundSO clip, int channel = 0)
+    {
+        this.clip = clip;
+        this.channel = channel;
+            
+        return this;
+    }
+}
+
+public class StopSoundEvent : GameEvent
+{
+    public int channel;
+
+    public StopSoundEvent Initialize(int channel = 0)
+    {
+        this.channel = channel;
+        return this;
+    }
+}
---
+++ Assets/ObjectPool/RunTime/PoolManagerMono.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Core;
+
+namespace KimMin.ObjectPool.RunTime
+{
+    public class PoolManagerMono : MonoSingleton<PoolManagerMono>
+    {
+        [SerializeField] private PoolManagerSO poolManager;
+
+        private void Awake()
+        {
+            poolManager.Initialize(transform);
+        }
+        
+        public T Pop<T>(PoolItemSO item) where T : IPoolable
+        {
+            return (T)poolManager.Pop(item);
+        }
+        
+        public void Push(IPoolable item)
+        {
+            poolManager.Push(item);
+        }
+    }
+}
